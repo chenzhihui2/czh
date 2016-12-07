e5cbd0e95cf4285843b05c042fd8b7608ca2edba
chenzhihui <453775810@qq.com>
2016-12-07 15:38:52 +0800

    delete verything

--- czh-common/src/test/java/TestMockito.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by czh on 2015/7/24.
- */
-
-public class TestMockito {
-}
--- czh-persistent/src/main/java/com/czh/persistent/pagePlugin/pagination/Dialect.java
+++
@@ -1,7 +0,0 @@
-package com.czh.persistent.pagePlugin.pagination;
-
-public interface Dialect {
-    public String getLimitSql(String sql, int offset, int limit);
-    
-    public String  getCountSql(String sql);
-}
